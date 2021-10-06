d26ad06390ed8327562f8364fca32f1726dd51d8
alexandre-zup <alexandre.santos@zup.com.br>
2021-10-06 16:11:34 -0300

    criado arquivo data.sql

---
+++ src/main/resources/data.sql
@@ -0,0 +1,3 @@
+INSERT INTO cliente(id, email, nome) VALUES ('c20bb227-14b9-4ead-a43a-d235b60face7', 'enzo@hotmail.com', 'Enzo')
+INSERT INTO cliente(id, email, nome) VALUES ('87419655-8aab-4f18-886a-515c0406bd39', 'valentina@gmail.com', 'Valentina')
+INSERT INTO cliente(id, email, nome) VALUES ('7d6083de-041b-40c3-a958-9151f2161708', 'schumacher@gmail.com', 'Schumacher')
