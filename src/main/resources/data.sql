31bab90493e6a933dfe656d92932f504ae9b05e3
alexandre-zup <alexandre.santos@zup.com.br>
2021-10-06 16:18:12 -0300

    Update data.sql

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,3 +1,6 @@
-INSERT INTO cliente(id, email, nome) VALUES ('c20bb227-14b9-4ead-a43a-d235b60face7', 'enzo@hotmail.com', 'Enzo')
-INSERT INTO cliente(id, email, nome) VALUES ('87419655-8aab-4f18-886a-515c0406bd39', 'valentina@gmail.com', 'Valentina')
-INSERT INTO cliente(id, email, nome) VALUES ('7d6083de-041b-40c3-a958-9151f2161708', 'schumacher@gmail.com', 'Schumacher')
+-- c20bb227-14b9-4ead-a43a-d235b60face7
+-- 7d6083de-041b-40c3-a958-9151f2161708
+-- 87419655-8aab-4f18-886a-515c0406bd39
+INSERT INTO cliente(id, email, nome) VALUES ('c20bb22714b94eada43ad235b60face7', 'enzo@hotmail.com', 'Enzo')
+INSERT INTO cliente(id, email, nome) VALUES ('7d6083de041b40c3a9589151f2161708', 'schumacher@gmail.com', 'Schumacher')
+INSERT INTO cliente(id, email, nome) VALUES ('874196558aab4f18886a515c0406bd39', 'valentina@gmail.com', 'Valentina')
