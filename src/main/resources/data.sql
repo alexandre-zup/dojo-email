af85a2936f2b614fff959f23ae74efdc5f8b3192
Alisson Prado <alisson.prado@zup.com.br>
2021-10-07 14:18:04 -0300

    Afetuados testes finais

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,6 +1,7 @@
 -- c20bb227-14b9-4ead-a43a-d235b60face7
 -- 7d6083de-041b-40c3-a958-9151f2161708
 -- 87419655-8aab-4f18-886a-515c0406bd39
-INSERT INTO cliente(id, email, nome) VALUES ('c20bb22714b94eada43ad235b60face7', 'enzo@hotmail.com', 'Enzo')
+-- e8dee5fc-3099-416e-a92a-2d33ff10b226
+INSERT INTO cliente(id, email, nome) VALUES ('e8dee5fc3099416ea92a2d33ff10b226', 'enzo@hotmail.com', 'Enzo')
 INSERT INTO cliente(id, email, nome) VALUES ('7d6083de041b40c3a9589151f2161708', 'schumacher@gmail.com', 'Schumacher')
 INSERT INTO cliente(id, email, nome) VALUES ('874196558aab4f18886a515c0406bd39', 'valentina@gmail.com', 'Valentina')
